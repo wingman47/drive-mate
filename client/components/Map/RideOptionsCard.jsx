31192c970ecfdf15dee7d69b840cfca27291e118
zaidiyazdan <117353242+zaidiyazdan@users.noreply.github.com>
2023-12-21 21:06:42 +0530

    Adde

--- client/components/Map/RideOptionsCard.jsx
+++ client/components/Map/RideOptionsCard.jsx
@@ -14,7 +14,6 @@ import {useNavigation} from '@react-navigation/native';
 import {useSelector} from 'react-redux';
 import {selectTravelTimeInformation} from '../../slice/navSlice';
 import SubmitButton from '../Form/SubmitButton';
-import Navigation from '../../Navigation';
 
 const data = [
   {
@@ -42,8 +41,6 @@ const data = [
 
 const RideOptionsCard = () => {
   const navigation = useNavigation();
-  const [selected, setSelected] = useState(null);
-  const travelTimeInformation = useSelector(selectTravelTimeInformation);
 
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
@@ -58,42 +55,6 @@ const RideOptionsCard = () => {
           handleSubmit={() => navigation.navigate('DriverForm')}
           loading={false}
         />
-        {/* <TouchableOpacity style={tw`absolute top- z-50 3 left-5 rounded-full`}>
-                <Icon name="chevron-left" type="fontawesome" 
-                onPress={()=>navigation.navigate('NavigateCard')}/>
-            </TouchableOpacity>
-        <Text style={tw`text-center py-5 text-xl`}>Select a Ride - {travelTimeInformation?.distance.Text}</Text>
-        </View>
-        <FlatList
-    data={data}
-    keyExtractor={(item) => item.id}
-    renderItem={({ item: { id, title, multiplier, image }, item }) => (
-        <TouchableOpacity
-            style={tw`flex-row justify-between m-2 items-center px-10 ${id === selected?.id && "bg-gray-200"}`}
-            onPress={() => setSelected(item)}
-        >
-            <Image
-                style={{
-                    width: 80,
-                    height: 70,
-                    resizeMode: "contain",
-                }}
-                source={{
-                    uri: image,
-                }}
-            />
-            <View style={tw`-ml-6`}>
-                <Text style={tw`text-xl font-semibold`}>{title}</Text>
-                <Text>{travelTimeInformation?.duration.text}</Text>
-            </View>
-            <Text style={tw`text-xl`}>$239</Text>
-        </TouchableOpacity>
-    )}
-/>
-    <View>
-        <TouchableOpacity disabled={!selected} style={tw`bg-black py-3 m-3 rounded-lg ${!selected && "bg-gray-300"}`}>
-            <Text style={tw`text-center text-white text-xl`}>Choose {selected?.title}</Text>
-        </TouchableOpacity> */}
       </View>
     </SafeAreaView>
   );
--- client/screens/profile/ProfileScreen.jsx
+++ client/screens/profile/ProfileScreen.jsx
@@ -1,12 +1,67 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const ProfileScreen = () => {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          source={{ uri: 'https://placekitten.com/200/200' }}
+          style={styles.profileImage}
+        />
+        <Text style={styles.username}>User name</Text>
+      </View>
 
-export default ProfileScreen
+      <View style={styles.infoContainer}>
+        <Text style={styles.label}>Email:</Text>
+        <Text style={styles.infoText}>example@gmail.com</Text>
+
+        <Text style={styles.label}>Phone:</Text>
+        <Text style={styles.infoText}>+91 8294579845</Text>
+
+        <Text style={styles.label}>Location:</Text>
+        <Text style={styles.infoText}>City, Country</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+  header: {
+    alignItems: 'center',
+    padding: 30,
+    backgroundColor: '#007bff',
+  },
+  profileImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    borderWidth: 2,
+    borderColor: '#fff',
+  },
+  username: {
+    marginTop: 15,
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  infoContainer: {
+    padding: 30,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 10,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  infoText: {
+    fontSize: 18,
+    marginBottom: 15,
+    color: '#666'
+  }
+})
+
+export default ProfileScreen;
